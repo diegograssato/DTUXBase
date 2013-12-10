eb7f85e829615814dd8e99912a417c8ae478dc37
Diego Pereira Grassato <diego@kees.com.br>
2013-12-10 08:43:21 -0200

    Início do Abstract Form

---
+++ src/DTUXBase/Form/AbstractForm.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Classe responsável por
+ * @todo Classe responsável por
+ * @category Core
+ * @package Core
+ * @subpackage Admin
+ * @author Diego Pereira Grassato <diego.grassato@gmail.com>
+ * @data 10/12/13 08:21
+ */
+
+namespace DTUXBase\Form;
+
+
+class AbstractForm {
+
+} 
